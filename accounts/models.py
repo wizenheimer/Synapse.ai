3970269271ca79bcdf335bd1ccabdeb2c00d81b7
kn4y4n <kn4y4nnn@gmail.com>
2023-04-21 19:19:24 +0530

    feat: create user model

---
+++ accounts/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from .managers import UserManager
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, db_index=True)
+    # has verified email address
+    is_verified = models.BooleanField(default=False)
+    # has an active account
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return str(self.email)
