c52f3884f039a8e1fa98e5853a0b1865414c8170
kn4y4n <kn4y4nnn@gmail.com>
2023-04-26 23:39:16 +0530

    feat: add permission utils

--- snippets/permissions.py
+++ snippets/permissions.py
@@ -1,17 +1,12 @@
 from rest_framework.permissions import BasePermission, IsAuthenticated, SAFE_METHODS
 
 
-class ReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        return request.method in SAFE_METHODS
-
-
 class IsMember(BasePermission):
     def has_object_permission(self, request, view, obj):
         user = request.user
-        team = obj
+        snippet = obj
         if user.is_superuser:
             return True
-        if team.users.filter(id=user.id).exists():
+        if snippet.users.filter(id=user.id).exists():
             return True
         return False
